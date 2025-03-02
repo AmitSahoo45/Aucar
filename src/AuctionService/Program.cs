a2c39bbf1b5c4030d1e879948ce6fef2ccabe2e0
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2025-03-02 22:19:59 +0530

    Update bidding service & integrated gRPC

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,5 +1,6 @@
 using AuctionService.Consumers;
 using AuctionService.Data;
+using AuctionService.Services;
 using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -31,7 +32,7 @@ builder.Services.AddMassTransit(x =>
             h.Username(builder.Configuration.GetValue("RabbitMQ:Username", "guest")!);
             h.Password(builder.Configuration.GetValue("RabbitMQ:Password", "guest")!);
         });
-        
+
         cfg.ConfigureEndpoints(context);
     });
 });
@@ -44,6 +45,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.ValidateAudience = false;
         options.TokenValidationParameters.NameClaimType = "username";
     });
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
@@ -51,6 +53,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGrpcService<GrpcAuctionService>();
 
 try
 {
